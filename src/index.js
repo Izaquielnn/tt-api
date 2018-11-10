1f82b797c32933e00fabf9dd9737520768f02426
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-11-09 21:48:14 -0300

    add real time interaction with socket.io

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(
     "mongodb://tt:tt12345@ds157853.mlab.com:57853/tt-backend", 
     {
     useNewUrlParser: true
     }
 );
+
+app.use((req, res, next) =>{
+    req.io = io;
+    
+    return next();
+});
 
 app.use(express.json());
 app.use(require('./routes'));
