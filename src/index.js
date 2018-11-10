7d627533493895fe62b37dd11e23b9c67b7e9ca5
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-11-09 21:11:06 -0300

    add banco de dados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect(
+    "mongodb://tt:tt12345@ds157853.mlab.com:57853/tt-backend", 
+    {
+    useNewUrlParser: true
+    }
+);
 
 app.get('/', (req, res) => {
     return res.send('Hello World');
